304fe6fd87bd83d694e2f3f5c1632e9b72d81331
Nadav Ivgi <nadav@shesek.info>
2014-07-01 19:16:46 +0300

    Set created_at only when isNew
    Otherwise, loading documents without the "created_at" field and saving them causes the creation time to be re-set to the current time.
    
    Using isNew is more accurate and can prevent that from happening.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function timestamps(opts) {
     schema.add({ updated_at: Date, created_at: Date });
     schema.pre('save', function(next){
       var timestamp = new Date();
-      this.created_at = this.created_at || timestamp;
+      this.isNew && (this.created_at = timestamp);
       this.updated_at = timestamp;
       next();
     });
